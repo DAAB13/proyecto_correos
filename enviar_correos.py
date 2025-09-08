73a599e201d080b8bb6ca8d0874b6a8b13eac9c4
DAAB13 <diego.ab130@gmail.com>
2025-09-07 20:28:27 -0500

    lectura de archivo xlsx y organizar correos

---
+++ enviar_correos.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+### -------- Estación 1: leer y entender los datos ------------
+nombre_archivo = 'data/tabla_accesos.xlsx'
+try:
+  df = pd.read_excel(nombre_archivo) # pandas lee el archivo xlsx
+  print(df.head())
+
+
+  ### -------- Estación 2: organizar por instructor ------------
+  """ el método .unique sirve para obtener una lista de correo sin repetir """
+  correos_intructores = df['Correo Institucional'].unique() 
+  print("---Instructores a contactar---")
+  print(correos_intructores)
+except FileNotFoundError:
+  print(f"no se encontro el archivo:\n{nombre_archivo}\nAsegurate que exista el archivo")

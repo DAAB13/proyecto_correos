16accf57804ccdb6a6bc5dc009789ba82f02502c
DAAB13 <diego.ab130@gmail.com>
2025-09-07 22:29:23 -0500

    estación 3: en desarrollo

--- enviar_correos.py
+++ enviar_correos.py
@@ -4,13 +4,21 @@ import pandas as pd
 nombre_archivo = 'data/tabla_accesos.xlsx'
 try:
   df = pd.read_excel(nombre_archivo) # pandas lee el archivo xlsx
-  print(df.head())
-
-
-  ### -------- Estación 2: organizar por instructor ------------
+  #print(df.head())
+### -------- Estación 2: organizar por instructor ------------
   """ el método .unique sirve para obtener una lista de correo sin repetir """
   correos_intructores = df['Correo Institucional'].unique() 
   print("---Instructores a contactar---")
-  print(correos_intructores)
+  #print(correos_intructores)
 except FileNotFoundError:
   print(f"no se encontro el archivo:\n{nombre_archivo}\nAsegurate que exista el archivo")
+
+
+### -------- Estación 3: Construir mensaje personalizado ------------
+for correo in correos_intructores:
+  """filtrar datos para un instructor específico"""
+  df_instructor = df[df['Correo Institucional'] == correo] 
+  print(df_instructor)
+  nombre_instructor = df_instructor['Instructor'].iloc[0] #iloc toma la primera fila de la selección
+  tabla_para_correo = df_instructor.drop(columns=['Correo Institucional'])
+  print(tabla_para_correo)

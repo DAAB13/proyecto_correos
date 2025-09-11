751dd7957a4936c8892ea04ea410677abc86f1f2
DAAB13 <diego.ab130@gmail.com>
2025-09-10 22:56:44 -0500

    Versión 1.0 - running

---
+++ config.py
@@ -0,0 +1,3 @@
+NOMBRE_ARCHIVO = 'data/tabla_accesos.xlsx'
+ASUNTO_DEL_CORREO = "Accesos a cuentas Zoom - Cursos de {mes}"
+NOMBRE_REMITENTE = "Centro de Idiomas UPCH"
--- enviar_correos.py
+++ enviar_correos.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import config
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
 
 ### -------- Estación 1: leer y entender los datos ------------
-nombre_archivo = 'data/tabla_accesos.xlsx'
+load_dotenv()
+mi_correo = "diego.alvarado@upch.pe"
+mi_contraseña = os.getenv("GMAIL_APP_PASSWORD")
+
 try:
-  df = pd.read_excel(nombre_archivo) # pandas lee el archivo xlsx
+  df = pd.read_excel(config.NOMBRE_ARCHIVO) # pandas lee el archivo xlsx
   #print(df.head())
 ### -------- Estación 2: organizar por instructor ------------
   """ el método .unique sirve para obtener una lista de correo sin repetir """
@@ -11,7 +19,7 @@ try:
   #print("---Instructores a contactar---")
   #print(correos_intructores)
 except FileNotFoundError:
-  print(f"no se encontro el archivo:\n{nombre_archivo}\nAsegurate que exista el archivo")
+  print(f"no se encontro el archivo:\n{config.NOMBRE_ARCHIVO}\nAsegurate que exista el archivo")
 
 
 ### -------- Estación 3: Construir mensaje personalizado ------------
@@ -34,7 +42,22 @@ for correo in correos_intructores:
       <p>Te compartimos sus accesos para el mes de {nombre_mes}:</p> 
       {tabla_html}
       <p>Saludos cordiales</p>
-  <body>
-  <html>
+  </body>
+  </html>
   """
-  print(cuerpo_correo)
+  
+
+  ### -------- Estación 4: Enviar correo ------------
+  try: 
+    msg = EmailMessage()
+    msg['Subject'] = config.ASUNTO_DEL_CORREO.format(mes=nombre_mes)
+    msg['From'] = mi_correo
+    msg['To'] = "diego.ab130@gmail.com" # cambiar por = correo
+    msg.set_content(cuerpo_correo, subtype='html')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+      smtp.login(mi_correo, mi_contraseña)
+      smtp.send_message(msg)
+      print(f"Correo enviado exitosamente a {correo}")
+  except Exception as e:
+    print(f"Error al enviar correo a {correo}: {e}")
